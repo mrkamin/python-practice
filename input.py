95c7d3ea15b18fa7f5874afbedb7f0003280fbf5
Rafi <mrkamin2@gmail.com>
2025-01-29 08:45:08 +0500

    Practice input fun two question

--- input.py
+++ input.py
@@ -1,2 +1,5 @@
-name = input('what is your name? ')
-print("Hi " + name)
+full_name = input('What is your full name? ')
+
+
+fav_Color = input("What is your Favourite Color? ")
+print(full_name + " Likes " + fav_Color)
