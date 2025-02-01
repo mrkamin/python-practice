0067325c3b2d6aad5ea5601c4d2f1afd0a36fb02
Rafi <mrkamin2@gmail.com>
2025-02-01 10:10:09 +0500

    Practicing while loop statement example

--- if.py
+++ if.py
@@ -1,5 +1,14 @@
-i = 1
-while i <= 10:
-    print('*' * i)
-    i = i + 1
-print("Done")
+from wsgiref.util import guess_scheme
+
+secret_num = 9
+count_num = 0
+count_limit = 3
+
+while count_num < count_limit:
+    guess = int(input('Guess? '))
+    count_num += 1
+    if guess == secret_num:
+        print('You Won!')
+        break
+else:
+    print('Sorry You Faild!')
