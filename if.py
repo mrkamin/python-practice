eeddc47bbd537d8bc5c232d3673fa1465fe39d86
Rafi <mrkamin2@gmail.com>
2025-02-02 10:46:14 +0500

    Practicing while loop statement example

--- if.py
+++ if.py
@@ -1,26 +1,27 @@
 command = ""
 started = False
+
 while True:
-    command = input('>').lower()
+    command = input(">").lower()
     if command == "start":
         if started:
-            print("Car is already started!")
+            print("Car is already started...")
         else:
             started = True
-            print("Car started")
+            print("Car is started...")
     elif command == "stop":
         if not started:
             print("Car is already stopped!")
         else:
             started = False
-            print("Car Stoped")
+            print("Car is stopped!")
     elif command == "help":
         print("""
-Start - to start the car
-Stop - to stop the car
-quit - to exit
+start - to start the car!
+stop - to stop the car!
+quit - to exit the game!
         """)
-    elif command == "quit":
+    elif command == 'quit':
         break
     else:
-        print("I don't understand...")
+        print("I don't understand!")
