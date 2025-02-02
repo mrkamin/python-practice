331c8c9087b5d936ccd6112364df28262df43146
Rafi <mrkamin2@gmail.com>
2025-02-02 17:32:40 +0500

    Practicing for loop statement example

--- for.py
+++ for.py
@@ -1,7 +1,9 @@
-for item in "Rafi":
-    print(item)
 
-for item in [1,2,3,4,5,6]:
-    print(item)
-for item in range(10):
-    print(item)
+
+prices = [10, 20, 30]
+
+total = 0
+
+for price in prices:
+    total += price
+print(total)
