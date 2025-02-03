f4afa76c0f6f0feff90edfc92e3cee98a3589e87
Rafi <mrkamin2@gmail.com>
2025-02-03 17:57:10 +0500

    Practicing list excersize

--- list.py
+++ list.py
@@ -1,4 +1,11 @@
-numbers = [8, 2, 5, 2, 2]
+numbers = [7, 2, 5, 2, 2]
+for x_count in numbers:
+    print("x" * x_count)
 
-for number in numbers:
-    print('x' * number)
+numbers = 10
+for number in range(numbers):
+    print(number)
+items = ["gold", 'plastic', 'selver']
+
+for item in items:
+    print(item)
