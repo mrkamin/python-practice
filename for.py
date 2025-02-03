2d9373b08f6d6a2cfa6784c7e3b626758ae167ee
Rafi <mrkamin2@gmail.com>
2025-02-03 09:45:30 +0500

    Practicing for loop statement example

--- for.py
+++ for.py
@@ -1,4 +1,7 @@
-numbers = [7,2, 5, 2, 2 ]
+numbers = [7, 2, 5, 2, 2]
 
 for x_count in numbers:
-    print("x" * x_count)
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
