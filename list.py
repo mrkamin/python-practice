b7ed9bdb4f4b05c5c8c0336f1b205f562f9ce6ba
Rafi <mrkamin2@gmail.com>
2025-02-03 16:59:27 +0500

    Practicing list excersize

--- list.py
+++ list.py
@@ -1,9 +1,4 @@
-
-numbers = [20, 30,10,90,100,50,70,80,40]
-
-max = numbers[0]
+numbers = [8, 2, 5, 2, 2]
 
 for number in numbers:
-    if max > number:
-        max = number
-print(max)
+    print('x' * number)
