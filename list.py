c57546d7d7045a07face14d2873d31612f6a37d3
Rafi <mrkamin2@gmail.com>
2025-02-04 09:32:57 +0500

    Practicing list excersize

--- list.py
+++ list.py
@@ -1,22 +1,13 @@
-numbers = [7, 2, 5, 2, 2]
-for x_count in numbers:
-    print("x" * x_count)
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
 
-numbers = 10
-for number in range(numbers):
-    print(number)
-items = ["gold", 'plastic', 'selver']
+for row in matrix:
+    print(row)
+    for item in row:
+        print(row)
+        print(item)
 
-for item in items:
-    print(item)
-
-numbers = [1,2,3,4,5,6,7,10]
-for number in numbers:
-    print(number)
-
-max = numbers[0]
-
-for number in numbers:
-    if max < number:
-        max = number
-print(max)
+    print(row)
