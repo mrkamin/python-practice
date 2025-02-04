947ff2c3f27fb7499eb53c87858fe06e28645a8f
Rafi <mrkamin2@gmail.com>
2025-02-04 18:52:40 +0500

    Practicing tupple excersize

--- list.py
+++ list.py
@@ -1,7 +1,18 @@
-cordinates = (1,2,3)
+phone = input("Phone:")
 
-x,y,z = cordinates
+digit_mapping = {
+    '1' : 'One',
+    '2' : 'Two',
+    '3' : 'Three',
+    '4' : 'Four',
+    '5' : 'Five',
+    '6' : 'Six',
+    '7' : 'seven',
+    '8' : 'eight',
+    '9' : 'nine'
+}
+output = ""
+for ch in phone:
+    output += digit_mapping.get(ch, "!") + " "
 
-print(x)
-print(y)
-print(z)
+print(output)
