0cd9571d2688d720ba2119ed87ac9e570de3df1c
Rafi <mrkamin2@gmail.com>
2025-02-05 18:15:59 +0500

    Practicing tupple excersize

--- list.py
+++ list.py
@@ -1,18 +1,12 @@
-phone = input("Phone:")
+message = input(">")
 
-digit_mapping = {
-    '1' : 'One',
-    '2' : 'Two',
-    '3' : 'Three',
-    '4' : 'Four',
-    '5' : 'Five',
-    '6' : 'Six',
-    '7' : 'seven',
-    '8' : 'eight',
-    '9' : 'nine'
+words = message.split(" ")
+
+emojis = {
+    ":)": "Happy emoje",
+    ":(" :  "sad emojy"
 }
 output = ""
-for ch in phone:
-    output += digit_mapping.get(ch, "!") + " "
-
+for word in words:
+   output += emojis.get(word, word) + " "
 print(output)
